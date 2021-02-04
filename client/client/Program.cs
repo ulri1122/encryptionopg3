a970e5de941fbf043a76e309f6ba091bab3835ab
ulrik <ulri1122@gmail.com>
2021-02-04 09:19:41 +0100

    first

---
+++ client/client/Program.cs
@@ -0,0 +1,63 @@
+﻿using encryption;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client
+{
+
+    class Program
+    {
+        public static Encryption enc = new Encryption();
+        public static string key = "jegharmangekeys";
+        public static string privatekey = "24";
+        public static string gkey = "";
+
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            int port = 13356;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(ip, port);
+
+            client.Connect(endPoint);
+
+            NetworkStream stream = client.GetStream();
+            Console.WriteLine("skriv besked til server?\n\"c\" to cancel");
+            RecieveMessage(stream);
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "c")
+                {
+                    break;
+                }
+                byte[] buffer = Encoding.UTF8.GetBytes(userInput);
+
+                stream.Write(enc.EncryptByte(buffer, key), 0, buffer.Length);
+
+            }
+            client.Close();
+        }
+
+        public static async void RecieveMessage(NetworkStream stream)
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[256];
+                int nBytesRead = await stream.ReadAsync(buffer, 0, 256);
+
+
+
+                string message = Encoding.UTF8.GetString(enc.DecryptByte(buffer, key), 0, nBytesRead);
+                if (message != string.Empty)
+                {
+                    Console.WriteLine(message);
+                }
+            }
+
+        }
+    }
+}
---
+++ encryption/encryption/encryption.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace encryption
+{
+
+    public class Encryption
+    {
+
+        public byte[] EncryptByte(byte[] bytes, string key)
+        {
+
+            byte[] bkey = Encoding.ASCII.GetBytes(key);
+            /*
+            double byteslength = Math.Ceiling(Convert.ToDouble(bytes.Length) / 4);
+            byte[,] bbytes2d = new byte[Convert.ToInt32(byteslength), 4];
+            int c = 0;
+            for (int j = 0; j < bbytes2d.GetLength(0); j++)
+            {
+                for (int k = 0; k <= bbytes2d.Length; k++)
+                {
+                    if (c >= bytes.Length)
+                    {
+                        c = 0;
+                        bbytes2d[j, k] = bytes[Convert.ToInt32(c)];
+                        break;
+                    }
+                    bbytes2d[j, k] = bytes[c];
+
+                    c++;
+                }
+            }
+
+            */
+            int a = 0;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (a >= bkey.Length - 1)
+                {
+                    a = 0;
+                }
+                int change = i + bkey[a];
+
+                while (change + bytes[i] > 255) change -= 256;
+                bytes[i] += (byte)change;
+                bytes[i] += bkey[a];
+
+
+                a++;
+            }
+
+
+
+            return bytes;
+        }
+
+        public byte[] DecryptByte(byte[] bytes, string key)
+        {
+            byte[] bkey = Encoding.ASCII.GetBytes(key);
+            /*
+            int arraylength = 0;
+            for (int b = 0; b < bytes.Length; ++b)
+            {
+                if (bytes[b] == 0)
+                {
+                    arraylength = b;
+                    break;
+
+                }
+            }
+            Array.Resize(ref bytes, arraylength);
+            double byteslength = Math.Ceiling(Convert.ToDouble(bytes.Length) / 4);
+
+            byte[,] bbytes2d = new byte[Convert.ToInt32(byteslength), 4];
+            int c = 0;
+            for (int j = 0; j < bbytes2d.GetLength(0); j++)
+            {
+                for (int k = 0; k <= bbytes2d.Length; k++)
+                {
+                    if (c >= bytes.Length)
+                    {
+                        c = 0;
+                        bbytes2d[j, k] = bytes[Convert.ToInt32(c)];
+                        break;
+                    }
+                    bbytes2d[j, k] = bytes[c];
+
+                    c++;
+                }
+            }
+            */
+            int a = 0;
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                if (a >= bkey.Length - 1)
+                {
+                    a = 0;
+                }
+                int change = i + bkey[a];
+                while (change - bytes[i] < 0) change += 256;
+                bytes[i] -= (byte)change;
+                bytes[i] -= bkey[a];
+                a++;
+            }
+            return bytes;
+
+        }
+
+        public void PrintBytesUTF8(byte[] bytes)
+        {
+            string toPrint = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine(toPrint);
+        }
+    }
+}
